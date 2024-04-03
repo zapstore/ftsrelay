fb7682f158f53672e3411c206eb272b6b12f77ab
franzap <_@franzap.com>
2024-04-03 18:23:57 -0300

    Implement subscriptions

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const blossomDir = Bun.env.BLOSSOM_DIR ?? '/tmp';
 
 $.cwd(blossomDir);
 
+// Keys uniquely identify a connection with `${wid}-${reqId}`,
+// where `wid` is `ws.data.createdAt` and `reqId` the nostr request ID
+const subIds = {};
+
 const server = Bun.serve({
   async fetch(req, server) {
     const pathname = new URL(req.url).pathname;
@@ -19,11 +23,15 @@ const server = Bun.serve({
 
     if (pathname === '/') {
       // upgrade connection for ws
-      server.upgrade(req);
+      server.upgrade(req, {
+        data: {
+          createdAt: Date.now()
+        }
+      });
       // TODO NIP-11
       // see curl -i -H 'Accept: application/nostr+json' https://relay.damus.io
       // return new Response(Bun.file("./nip11.json"));
-      return new Response('relay.zap.store');
+      return new Response('Welcome to relay.zap.store');
     }
 
     if (/\/^[0-9a-f]{64}(\.\S{1,}|$)/.test(pathname)) {
@@ -37,29 +45,47 @@ const server = Bun.serve({
         return new Response(req.method == 'GET' ? file : null, { status: exists ? 200 : 404, headers });
       }
     } else {
-      return Response('not found', { status: 404 });
+      return Response('Not found', { status: 404 });
     }
   },
+
   websocket: {
     async message(ws, message) {
       const [type, payload, filter] = JSON.parse(message);
       switch (type) {
-        // TODO implement subscriptions after EOSE
-        case 'REQ': return _handleRequest(payload, filter, ws);
-        case 'EVENT': return _handleEvent(payload, ws);
-        case 'CLOSE': return ws.send(JSON.stringify(["CLOSED", payload]));
-        default: return ws.send(JSON.stringify(["NOTICE", `error: wtf you mean by ${type}`]));
+        case 'REQ': return _handleRequest(ws, payload, filter);
+        case 'EVENT': return _handleEvent(ws, payload);
+        case 'CLOSE': return _handleClose(ws, payload);
+        default: return _handleError(ws, type);
       }
     },
+    async close(ws) {
+      // When socket is closed, remove all it reqId subscriptions
+      for (const subId of Object.keys(subIds)) {
+        if (subId.startsWith(ws.data.createdAt)) {
+          delete subIds[subId];
+        }
+      }
+    },
+    // Necessary to send data back to the socket when using server.publish!
+    publishToSelf: true,
   },
 });
 
 const filterMappings = { ids: 'id', authors: 'pubkey', kinds: 'kind' };
 const slRegex = /^#[A-Za-z]$/;
 
-function _handleRequest(payload, filter, ws) {
+function _handleRequest(ws, reqId, filter) {
+  const subId = ws.data ? `${ws.data.createdAt}-${reqId}` : `${ws}-${reqId}`;
   const wheres = [];
   const params = {};
+
+  // Set up subscription upon initial request (when ws is an object, not a string),
+  // register filter in subIds
+  if (ws.data) {
+    ws.subscribe(subId);
+    subIds[subId] = filter;
+  }
 
   for (const [filterKey, filterValue] of Object.entries(filter)) {
     if (Array.isArray(filterValue)) {
@@ -82,7 +108,6 @@ function _handleRequest(payload, filter, ws) {
     }
   }
 
-  // since, until
   if (typeof filter.since == 'number') {
     wheres.push(`created_at >= $since`);
     params.$since = filter.since;
@@ -98,6 +123,11 @@ function _handleRequest(payload, filter, ws) {
     params.$search = filter.search.replace(/[^\w\s]|_/gi, " ");
   }
 
+  // Do not EOSE during updates (!ws.data)
+  if (ws.data && wheres.length == 0) {
+    return server.publish(subId, JSON.stringify(["EOSE", reqId]));
+  }
+
   let query = `SELECT events.id, pubkey, sig, kind, created_at, content, tags
       FROM events ${params.$letter ? ", json_each(tags)" : ''}
       WHERE ${wheres.join(' AND ')}  ${filter.search ? '' : 'ORDER BY created_at DESC'}`;
@@ -108,57 +138,63 @@ function _handleRequest(payload, filter, ws) {
     params.$limit = filter.limit;
   }
 
-  // ws.send(`${query} ---- ${JSON.stringify(params)}`);
+  // server.publish(subId, `${query} ---- ${JSON.stringify(params)}`);
   const events = db.query(query).all(params);
 
   for (const e of events) {
-    ws.send(JSON.stringify(["EVENT", payload, _deserialize(e)]));
+    server.publish(subId, JSON.stringify(["EVENT", reqId, _deserialize(e)]));
   }
-  ws.send(JSON.stringify(["EOSE", payload]));
+
+  // Do not EOSE during updates (!ws.data)
+  if (ws.data) {
+    server.publish(subId, JSON.stringify(["EOSE", reqId]));
+  }
 }
 
-function _handleEvent(payload, ws) {
+async function _handleEvent(ws, payload) {
   try {
     const isValid = _validateEvent(payload);
     if (isValid) {
       const existsQuery = `SELECT EXISTS(SELECT 1 from events where id = $id) as result;`;
-      const exists = db.query(existsQuery).get({ '$id': payload.id });
-      // only process if it does not exist
-      if (!exists.result) {
+      const exists = db.query(existsQuery).get({ $id: payload.id });
+
+      if (exists.result) {
+        return ws.send(JSON.stringify(["OK", payload.id, false, "duplicate"]));
+      } else {
         const [query, params] = _serialize(payload);
         db.query(query).run(params);
 
-        // keep file in local blossom
-        const url = _getFirstTag(payload.tags, 'url');
-        if (payload.kind === 1063 && url) {
-          const x = _getFirstTag(payload.tags, 'x');
-          fetch(url).then(async file => {
-            const arrayBuffer = await file.arrayBuffer();
-            const hasher = new Bun.CryptoHasher('sha256');
-            hasher.update(arrayBuffer);
-            const hash = hasher.digest('hex');
-            if (x === hash) {
-              // only store files under 200MB
-              if (arrayBuffer.byteLength < 200_000_000) {
-                try {
-                  const output = Bun.file(join(blossomDir, `${x}${extname(url)}`));
-                  await Bun.write(output, new Blob([arrayBuffer]));
-                } catch (e) {
-                  console.error(e);
-                }
-              }
-            } else {
-              // if hash doesn't match, remove garbage
-              db.query(`DELETE FROM events WHERE id = $id`).run({ '$id': payload.id });
-            }
-          }).catch(e => console.error(e));
+        ws.send(JSON.stringify(["OK", payload.id, isValid, ""]));
+
+        // For every new inserted event, notify all sockets with active requests
+        for (const subId of Object.keys(subIds)) {
+          const [wid, ...parts] = subId.split('-');
+          const reqId = parts.join('-');
+          const filter = subIds[subId];
+
+          // The trick here is reusing _handleRequest with a modified filter
+          // (we re-pass the original filter but limited to this new ID)
+          const updateFilter = { ...filter, ids: [payload.id] };
+          _handleRequest(wid, reqId, updateFilter);
+          await Bun.sleep(5); // wait 5ms betwen requests
         }
+
+        _saveInBlossom(payload);
       }
     }
-    ws.send(JSON.stringify(["OK", payload.id, isValid, ""]));
   } catch (e) {
     ws.send(JSON.stringify(["OK", payload.id, false, `error: ${e}`]));
   }
+}
+
+function _handleClose(ws, reqId) {
+  const subId = `${ws.data.createdAt}-${reqId}`;
+  delete subIds[subId];
+  ws.send(JSON.stringify(["CLOSED", reqId]));
+}
+
+function _handleError(ws, type) {
+  ws.send(JSON.stringify(["NOTICE", `error: wtf you mean by ${type}`]));
 }
 
 // 30063s and APK 1063s only accepted atm
@@ -171,7 +207,7 @@ const _validateEvent = (e) => {
   return e.kind == 30063;
 };
 
-const _getFirstTag = (tags, name) => tags.filter(t => t[0] == name)?.[0]?.[1];
+const _getFirstTag = (tags, name) => tags.find(t => t[0] == name)?.[1];
 
 function _serialize(obj) {
   const keys = Object.keys(obj);
@@ -183,5 +219,32 @@ function _deserialize(obj) {
   obj.tags = JSON.parse(obj.tags);
   return obj;
 }
+
+async function _saveInBlossom(payload) {
+  const url = _getFirstTag(payload.tags, 'url');
+  if (payload.kind === 1063 && url) {
+    const x = _getFirstTag(payload.tags, 'x');
+    const ext = extname(url);
+
+    const response = await fetch(url);
+    const name = join(blossomDir, `${x}${ext}`);
+    await Bun.write(name, response);
+    const [computedHash, newName] = _renameToHash(name);
+
+    if (x !== computedHash) {
+      // If hash doesn't match, remove garbage
+      db.query(`DELETE FROM events WHERE id = $id`).run({ $id: payload.id });
+      await $`rm $NAME'`.env({ NAME: newName }).quiet();
+    }
+  }
+}
+
+const _renameToHash = async (name) => {
+  const ext = extname(name);
+  const hash = await $`cat $NAME | shasum -a 256 | head -c 64`.env({ NAME: name }).text();
+  const hashName = `${hash}${ext}`;
+  await $`mv $SRC $DEST`.env({ SRC: name, DEST: hashName }).quiet();
+  return [hash, hashName];
+};
 
 console.log(`Listening on localhost:${server.port} and blossom mounted on: ${blossomDir}`);
