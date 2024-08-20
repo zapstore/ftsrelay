f147cfcaae3320a1e6a79fd03d8b6ab591ae4fe4
franzap <_@franzap.com>
2024-08-20 14:41:18 +0300

    Use CLOSED instead of EOSE

--- index.js
+++ index.js
@@ -154,8 +154,7 @@ function _handleRequest(ws, reqId, filters) {
       // NOTE: for now excluding any requests unrelated to zap.store
       if (!filter.kinds || !filter.kinds.some(k => [1011, 1063, 30063, 32267, 30267].includes(k))) {
         if (ws) {
-          server.publish(subId, JSON.stringify(["EOSE", reqId]));
-          return;
+          return ws.send(JSON.stringify(["CLOSED", reqId, '']));
         }
         return [];
       }
